d7e395676ac56e4865aba2b9effc8065fabfa790
mikesebald <michael.sebald@gmail.com>
2017-12-26 21:11:26 +0100

    Fix wip

--- app.R
+++ app.R
@@ -20,29 +20,50 @@ ui <- fluidPage(
       sidebarPanel(
         checkboxGroupInput("recordType",
                            "Record type",
-                           list("Golden records",
-                                "Valids",
-                                "Invalids")),
+                           list("Golden records" = "goldens",
+                                "Valid records" = "valids",
+                                "Invalid records" = "invalids")
+                           ),
         dateRangeInput("dateRange", 
                        "Date range",
                        start = "2017-01-01",
                        end = "2017-12-31",
                        min = "2017-01-01",
-                       max = "2017-12-31"),
+                       max = "2017-12-31"
+                       ),
         actionButton("updateAction",
-                     "Update")
+                     "Update"
+                     )
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-
+        textOutput("fromDate"),
+        textOutput("toDate"),
+        textOutput("recordType"),
+        textOutput("buttonPushed")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  output$fromDate <- renderText({
+    paste0("From: ", unlist(strsplit(as.character(input$dateRange), " to "))[1])
+  })
+  
+  output$toDate <- renderText({
+    paste0("To: ", unlist(strsplit(as.character(input$dateRange), " to "))[2])
+  })
+  
+  output$recordType <- renderText({
+    paste("Selected record types: ", paste0(input$recordType, collapse = ", "))
+  })
+  
+  output$buttonPushed <- renderText({
+    input$updateAction
+    paste(input$updateAction)
+  })
 }
 
 # Run the application 
