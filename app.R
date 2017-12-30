122a4f1e120d5d50348f686bab7ef87693fe39be
mikesebald <michael.sebald@gmail.com>
2017-12-30 21:46:29 +0100

    Looking at date range queries

--- app.R
+++ app.R
@@ -4,71 +4,81 @@ library(mongolite)
 mdbname <- "cdh"
 colname <- "record_history"
 mdb <- mongo(collection = colname, db = mdbname)
+mongo_revision <- mongo("revision", "cdh")
 
 ui <- dashboardPage(
   skin = "red",
   dashboardHeader(title = "Dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard"),
-      menuItem("Validation errors", tabName = "validationErrors")
-    )
-  ),
-
-  dashboardBody(
-    tabItems(
-      tabItem("dashboard",
-        fluidRow(
-          box("Record selection", background = "maroon",
-            checkboxGroupInput("recordType",
-                               "Record type",
-                               list("Golden records" = "goldens",
-                                    "Valid source records" = "valids",
-                                    "Invalid source records" = "invalids"
-                                    )
-            )
-          ),
-          box("Date range selection", background = "teal",
-            dateRangeInput("dateRange", 
-                             "Date range",
-                             start = "2017-01-01",
-                             end = "2017-12-31",
-                             min = "2017-01-01",
-                             max = "2017-12-31"
+  dashboardSidebar(sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard"),
+    menuItem("Validation errors", tabName = "validationErrors")
+  )),
+  
+  dashboardBody(tabItems(
+    tabItem(
+      "dashboard",
+      fluidRow(
+        box(
+          "Record selection",
+          background = "maroon",
+          checkboxGroupInput(
+            "recordType",
+            "Record type",
+            list(
+              "Golden records" = "goldens",
+              "Valid source records" = "valids",
+              "Invalid source records" = "invalids"
             )
           )
         ),
-        fluidRow(
-          valueBoxOutput("goldens"),
-          valueBoxOutput("valids"),
-          valueBoxOutput("invalids")
+        box(
+          "Date range selection",
+          background = "teal",
+          dateInput(
+            "fromDate",
+            "From",
+            value = "2017-01-01",
+            min = "2017-01-01",
+            max = "2017-12-31"
+          ),
+          dateInput(
+            "toDate",
+            "To",
+            value = "2017-12-31",
+            min = "2017-01-01",
+            max = "2017-12-31"
+          )
         )
       ),
-      tabItem("validationErrors")
-    )
-  )
+      fluidRow(
+        valueBoxOutput("goldens"),
+        valueBoxOutput("valids"),
+        valueBoxOutput("invalids")
+      )
+    ),
+    tabItem("validationErrors",
+            fluidRow())
+  ))
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$fromDate <- renderText({
-    paste0("From: ", unlist(strsplit(as.character(input$dateRange), " to "))[1])
-  })
-  
-  output$toDate <- renderText({
-    paste0("To: ", unlist(strsplit(as.character(input$dateRange), " to "))[2])
-  })
-  
-  output$recordType <- renderText({
-    paste("Selected record types: ", paste0(input$recordType, collapse = ", "))
-  })
-  
   output$goldens <- renderValueBox({
+    fromDate <- as.character(input$fromDate)
+    toDate <- as.character(input$toDate)
+    cat("from: ", fromDate, "\n")
+    cat("to: ", toDate, "\n")
+    
+    mongo_revision$find(query = '{"date": {"$gte": {"$date": "2017-01-04T00:00:00Z"}}}', 
+                        fields = '{"_id": 1}', 
+                        sort = '{"date": 1}', 
+                        limit = 1)
+
+
     if (!is.na(match("goldens", input$recordType)))
       recordCount <- mdb$count(query = '{"_id.s": "system"}')
     else
       recordCount <- 0
-
+    
     valueBox(
       value = as.character(recordCount),
       subtitle = "Golden records",
@@ -78,10 +88,11 @@ server <- function(input, output) {
   
   output$valids <- renderValueBox({
     if (!is.na(match("valids", input$recordType)))
-      recordCount <- mdb$count(query = '{"api": "merged", "_id.s": {"$ne": "system"}}')
+      recordCount <-
+        mdb$count(query = '{"api": "merged", "_id.s": {"$ne": "system"}}')
     else
       recordCount <- 0
-
+    
     valueBox(
       value = as.character(recordCount),
       subtitle = "Valid source records",
@@ -94,7 +105,7 @@ server <- function(input, output) {
       recordCount <- mdb$count(query = '{"api": "validated"}')
     else
       recordCount <- 0
-
+    
     valueBox(
       value = as.character(recordCount),
       subtitle = "Invalid source records",
@@ -103,6 +114,5 @@ server <- function(input, output) {
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
